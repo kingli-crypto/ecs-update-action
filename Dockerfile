9b25915877e1488874335fc260e0492e86599ca9
King Li <king.li@crypto.com>
2020-11-16 20:00:13 +0800

    Reduce args

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Container image that runs your code
 FROM amazon/aws-cli:2.1.1
 
-RUN curl -o /bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x /bin/jq
+RUN curl -o /bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x /bin/jq
 
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-TASK_FAMILY=$1
-TASK_REVISION=$2
-IMAGE_REPO_NAME=$3
-CODEBUILD_RESOLVED_SOURCE_VERSION=$4
-ECS_CLUSTER=$5
-SERVICE_NAME=$6
+TASK_REVISION=$1
+ECR_IMAGE=$2
+ECS_CLUSTER=$3
+SERVICE_NAME=$4
 
+TASK_FAMILY=$(aws ecs describe-services --services worker-ethereum --cluster STG-SECBB-BIPS-ADDSVC --query "services[0].taskDefinition" --output text | sed  "s/..*task-definition\/\(..*\):..*/\1/")
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
-
-ECR_IMAGE="$IMAGE_REPO_NAME:$CODEBUILD_RESOLVED_SOURCE_VERSION"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY:$TASK_REVISION" --region "$AWS_DEFAULT_REGION")
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 NEW_TASK_INFO=$(aws ecs register-task-definition --region "$AWS_DEFAULT_REGION" --cli-input-json "$NEW_TASK_DEFINTIION")
